79e617df6fe1437e26acaa2bf831f0e7de402209
ajeetrajbhar2505 <117207996+ajeetrajbhar2505@users.noreply.github.com>
2024-04-11 01:30:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const dotenv = require('dotenv')
 const http = require('http');
-const socketIo = require('socket.io');
+// const socketIo = require('socket.io');
 const chatSocket = require('./sockets/chatSocket');
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,8 +12,8 @@ app.use(express.json())
 dotenv.config()
 app.use(cors())
 
-const server = http.createServer(app);
-const io = socketIo(server);
+// const server = http.createServer(app);
+// const io = socketIo(server);
 
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes');
@@ -31,7 +31,7 @@ app.use('/api/messages',authorizeToken, messageRoutes);
 app.use('/api/rooms',authorizeToken, roomRoutes);
 
 // Socket connection
-io.on('connection', chatSocket);
+// io.on('connection', chatSocket);
 
 
 // Establish database connection
